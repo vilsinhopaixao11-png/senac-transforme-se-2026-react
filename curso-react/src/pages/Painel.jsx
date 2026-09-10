7fb2f6c477b78fa8372b6d776014896310ebbac3
vilsinhopaixao11-png <vilsinhopaixao11@gmail.com>
2026-09-09 22:17:16 -0300

    acrescentei a funcao para redirecionar

--- curso-react/src/pages/Painel.jsx
+++ curso-react/src/pages/Painel.jsx
@@ -1,21 +1,32 @@
-import { useState,useEffect } from 'react';
-import {Link} from 'react-router';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router';
 
-function Painel(){
-    const [modal, setModal ] = useState(false) //bollean
+function Painel() {
+    const [modal, setModal] = useState(false) //bollean
     const [users, setUsers] = useState([]) //vetor
     const [user, setUser] = useState({}) //objeto
-    const[logged, setLogged] = useState({})
+    const [logged, setLogged] = useState({})
 
     useEffect(
-        ()=>{
-            const logged =JSON.parse(localStorage.getItem('logged'))
+        () => {
+            const logged = JSON.parse(localStorage.getItem('logged'))
             setLogged(logged)
         },
         []
-    );    
+    );
 
-    function handleRegister(){
+    useEffect(() => {
+        const usersTemp = JSON.parse(localStorage.getItem('users')) || []
+        if (usersTemp) setUsers(usersTemp)
+    }, [])
+
+    function updateUser(pUser){
+        setModal(true)
+        setUser(pUser)
+
+    }
+
+    function handleRegister() {
         const newUsers = [...users, user]
         setUsers(newUsers)
         localStorage.setItem('users', JSON.stringify(newUsers))
@@ -23,58 +34,72 @@ function Painel(){
         setModal(false)
     }
 
-    return(
-<div>
- <h3 >Bem vindo , {logged?.nome}</h3>
+    return (
+        <div>
+            <h3 >Bem vindo , {logged?.nome}</h3>
 
-{ modal && (
-    <div 
-        className="fixed flex top-0 right-0 bottom-0 
+            {modal && (
+                <div
+                    className="fixed flex top-0 right-0 bottom-0 
             left-0 items-center justify-center bg-black/50 z-50">
 
-        <div className="relative max-w-md w-full p-5 bg-about rounded-lg 
+                    <div className="relative max-w-md w-full p-5 bg-about rounded-lg 
             shadow-md flex flex-col bg-white">
 
-            <a id="btClose" 
-                className="bg-prices absolute top-0 right-0 px-2 
+                        <a onClick={()=>setModal(false)}
+                            className="bg-prices absolute top-0 right-0 px-2 
                 rounded-full cursor-pointer">
-                X
-            </a>
+                            X
+                        </a>
 
-            <h2>Cadastre um novo usuário</h2>
-            <p>Preencha as informações abaixo</p>
-            
-            <form className="flex flex-col">
-                Nome:
-                <input onChange={ (e) => setUser({...user, nome: e.target.value }) } type="text" placeholder="Digite seu nome completo" />
-                Email:
-                <input onChange={ (e) => setUser({...user, email: e.target.value }) }  type="email" placeholder="Digite o seu melhor email" />
-    
-                Senha:
-                <input onChange={ (e) => setUser({...user, senha: e.target.value }) }  type="password" placeholder="Letra maiúscula e números" />
-                Data de nascimento:
-                <input onChange={ (e) => setUser({...user, nascimento: e.target.value }) }  type="date" />
-       
-                <a onClick={handleRegister} className="mt-5 bg-primary text-white text-center rounded-md py-2">Salvar</a>
-            </form>
+                        <h2>Cadastre um novo usuário</h2>
+                        <p>Preencha as informações abaixo</p>
+
+                        <form className="flex flex-col">
+                            Nome:
+                            <input value={user.nome} onChange={(e) => setUser({ ...user, nome: e.target.value })} type="text" placeholder="Digite seu nome completo" />
+                            Email:
+                            <input  value={user.email}  onChange={(e) => setUser({ ...user, email: e.target.value })} type="email" placeholder="Digite o seu melhor email" />
+
+                            Senha:
+                            <input onChange={(e) => setUser({ ...user, senha: e.target.value })} type="password" placeholder="Letra maiúscula e números" />
+                            Data de nascimento:
+                            <input onChange={(e) => setUser({ ...user, nascimento: e.target.value })} type="date" />
+
+                            <a onClick={handleRegister} className="mt-5 bg-primary text-white text-center rounded-md py-2">Salvar</a>
+                        </form>
+                    </div>
+                </div>
+            )}
+
+            <a onClick={() => setModal(true)} className="rounded-full bg-primary text-white px-4 py-3 fixed bottom-0 right-0"> + </a>
+
+            <table>
+                <thead>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Ações</th>
+                </thead>
+                <tbody className="font-secundary">
+                    {users.map(u => (
+
+                        <tr>
+                            <td>{u.nome}</td>
+                            <td>{u.email}</td>
+                            <td>
+                                <a onClick={()=> updateUser(u)} className="curson-pointer px-3 mx-4 hover:shadow shadow-md text-white rounl bg-green-500" >V</a>
+                    
+
+                                <a className="curson-pointer px-3 mx-4 hover:shadow shadow-md text-white rounl bg-red-500">X</a>
+                            </td>
+                        </tr>
+                    ))}
+
+
+                </tbody>
+            </table>
+
         </div>
-    </div>
-)}
-
-<a onClick={() => setModal(true)} className="rounded-full bg-primary text-white px-4 py-3 fixed bottom-0 right-0"> + </a>
-
-    <table>
-        <thead>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Ações</th>
-        </thead>
-        <tbody id="listUsers" className="font-secundary">
-           
-        </tbody>
-    </table>
-
-</div>
     )
 }
 
